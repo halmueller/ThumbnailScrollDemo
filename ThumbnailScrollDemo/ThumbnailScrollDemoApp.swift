92467c2fb83452b3840cf99bdfb92822a103fb51
Hal Mueller <halmueller@gmail.com>
2024-12-17 16:53:40 -0800

    Initial Commit

---
+++ ThumbnailScrollDemo/ThumbnailScrollDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThumbnailScrollDemoApp.swift
+//  ThumbnailScrollDemo
+//
+//  Created by Hal Mueller on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ThumbnailScrollDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
